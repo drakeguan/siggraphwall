83bdb3e3c6be9ffc14a3b274b42a7d38274f72cd
Shuen-Huei (Drake) Guan <drake.guan@gmail.com>
2013-08-10 23:38:20 +0800

    Make Tenjin work in 1st strike.

--- index_gen.py
+++ index_gen.py
@@ -3,33 +3,34 @@
 
 # built-in modules
 import os
+from pprint import pprint
 import sys
 
 # 3rd-party through PyPI
+import tenjin
+#tenjin.set_template_encoding('utf-8')  # optional (defualt 'utf-8')
+from tenjin.helpers import *
+from tenjin.html import *
 
 # local
 from bibpy import bib
 
 
-def inject(fout):
-    fout.write('<ul id="Grid">\n')
-    for fn in filter(lambda fn: fn.endswith('bib'), os.listdir('bibtex')):
-        parser = bib.Bibparser(bib.clear_comments(open(os.path.join('bibtex', fn), 'r').read()))
-        parser.parse()
-        data = parser.records
-        name = os.path.basename(fn)[:-4]
-        fout.write('<li class="mix"><img src="teaser_images/%s.jpg" width="320px" /></li>\n' % name)
-    fout.write('</ul>\n')
-
-
 def main(argv=sys.argv[:]):
     with open('index.html', 'w') as fout:
-        for line in open('index_template.html', 'r'):
-            if '<!-- teaser_images_anchor -->' in line:
-                inject(fout)
-            else:
-                fout.write(line)
-    return 0
+        items = []
+        for fn in filter(lambda fn: fn.endswith('bib'), os.listdir('bibtex')):
+            parser = bib.Bibparser(bib.clear_comments(open(os.path.join('bibtex', fn), 'r').read()))
+            parser.parse()
+            data = parser.records.values()[0]
+            data['teaser'] = 'teaser_images/%s.jpg' % os.path.splitext(fn)[0]
+            pprint(data)
+            print '========================================================'
+            items.append(data)
+        engine = tenjin.Engine(path=['views'], layout='_layout.pyhtml')
+        html = engine.render('items.pyhtml', {'items':items})
+        fout.write(html)
+
 
 if __name__ == '__main__':
     sys.exit(main())
