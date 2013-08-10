f63da8a1b99dba8d7ec64966f5b730016d55c593
Shuen-Huei (Drake) Guan <drake.guan@gmail.com>
2013-08-10 13:53:03 +0800

    Put simple code snippets of bibpy into index_gen.py

--- index_gen.py
+++ index_gen.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# built-in modules
 import os
 import sys
+
+# 3rd-party through PyPI
+
+# local
+from bibpy import bib
 
 
 def inject(fout):
     fout.write('<ul id="Grid">\n')
     for fn in filter(lambda fn: fn.endswith('bib'), os.listdir('bibtex')):
+        parser = bib.Bibparser(bib.clear_comments(open(os.path.join('bibtex', fn), 'r').read()))
+        parser.parse()
+        data = parser.records
         name = os.path.basename(fn)[:-4]
         fout.write('<li class="mix"><img src="teaser_images/%s.jpg" width="320px" /></li>\n' % name)
     fout.write('</ul>\n')
