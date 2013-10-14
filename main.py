79bfc706a2c0875b0c41650fe85a04733241e2cf
Shuen-Huei (Drake) Guan <drake.guan@gmail.com>
2013-10-14 14:24:36 +0800

    Clone staticjinja/__main__.py.

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from staticjinja import Renderer
+import os
+
+if __name__ == "__main__":
+    template_folder = os.path.join(os.getcwd(), 'templates')
+    renderer = Renderer(template_folder=template_folder)
+    renderer.run(debug=True, use_reloader=False)
