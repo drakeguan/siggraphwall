d441e2abe9530eaf7ab5befdb63a7d65ed228487
Shuen-Huei (Drake) Guan <drake.guan@gmail.com>
2013-10-14 15:14:59 +0800

    First trial of making use of staticjinja with manual bootstrap template.

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-from staticjinja import Renderer
 import os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+from staticjinja import Renderer
+
+# local
+from bibpy import bib
+
+
+def get_images():
+    images = []
+    for root, dirs, files in os.walk('bibtex'):
+        # shuffle(files)
+        for fn in filter(lambda fn: fn.endswith('bib'), files):
+            parser = bib.Bibparser(bib.clear_comments(open(os.path.join(root, fn), 'r').read()))
+            parser.parse()
+            data = parser.records.values()[0]
+            # teaser image url
+            data['teaser'] = 'teaser_images/%s.jpg' % os.path.splitext(fn)[0]
+            data['thumb'] = 'teaser_images/thumb/%s.jpg' % os.path.splitext(fn)[0]
+            images.append(data['thumb'])
+    return {'images': images}
+
+
+def main(argv=sys.argv[:]):
+    template_folder = os.path.join(os.getcwd(), 'templates')
+    # for image in get_images()['images']:
+    #     print type(image), image
+    renderer = Renderer(
+            template_folder=template_folder,
+            contexts=[
+                ('index.html', get_images),
+                ])
+    renderer.run(debug=True, use_reloader=False)
+    return 0
 
 if __name__ == "__main__":
-    template_folder = os.path.join(os.getcwd(), 'templates')
-    renderer = Renderer(template_folder=template_folder)
-    renderer.run(debug=True, use_reloader=False)
+    sys.exit(main())
